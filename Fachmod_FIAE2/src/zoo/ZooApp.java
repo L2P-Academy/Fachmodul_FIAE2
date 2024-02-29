59b28f7e32457ed52baedfa0130166f4f877e732
L2P-Academy <projekte@l2p-academy.com>
2024-02-29 11:14:07 +0100

    LookAndFeel auf Graphite, import jar

--- Fachmod_FIAE2/src/zoo/ZooApp.java
+++ Fachmod_FIAE2/src/zoo/ZooApp.java
@@ -1,8 +1,20 @@
 package zoo;
 
+import javax.swing.UIManager;
+import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
+
 public class ZooApp {
 
 	public static void main(String[] args) {
+		
+		try {
+			// setze auf ein wunderschönes Aussehen
+			UIManager.setLookAndFeel(new GraphiteLookAndFeel());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		Zoo zoo = new Zoo();
 		ZooView zooView = new ZooView();
 		ZooController zooController = new ZooController();
