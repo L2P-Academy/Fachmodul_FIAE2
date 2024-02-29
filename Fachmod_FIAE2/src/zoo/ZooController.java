167b2ab228facfd0e49a3497738c8b88fa87846e
KKlu114 <KKlu114@PC27191166.cc-college.gecs.loc>
2024-02-29 13:34:50 +0100

    Parameter übergeben, sellTicket Methode hinzugefügt

--- Fachmod_FIAE2/src/zoo/ZooController.java
+++ Fachmod_FIAE2/src/zoo/ZooController.java
@@ -1,12 +1,32 @@
 package zoo;
 
-public class ZooController {
+import java.util.Date;
+import java.util.List;
+import java.lang.Math;
 
-	public static void zooController() {
-			//Leere Methode, Static weil keine Objekte der Klasse notwendig (nur 1 Controller)
+public class ZooController {
+	private List<String> ticketType;
+	private Double ticketPrice;
+	private Date ticketDate;
+	private Zoo zoo;				//Variable zoo initialisieren
+	private ZooView zooView; 		//Variable zooView initialisieren
+	
+	
+	
+	public void zooController(Zoo zoo,ZooView zooView) {
+		this.zoo = zoo; 
+		this.zooView = zooView;
+		
+			
 	}
 
-	public static void addAnimal() {
-			//Leere Methode, Static weil keine Objekte der Klasse notwendig (keine Eigenen Daten in der Klasse)
+	public void addAnimal(String name, int age, String species, List<String> type) {
+			Animal animal = new Animal(name, age, species, type); //initialisieren eines neuen Objekts animal
+			zoo.addAnimal(animal);
+			zooView.displayAnimals();
+	}
+	
+	public void sellTicket(List<String> ticketType, Double ticketPrice, Date ticketDate) {
+		
 	}
 }
