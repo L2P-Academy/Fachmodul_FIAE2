be0b842657bb73f4eaa29a935799128203d1155b
L2P-Academy <projekte@l2p-academy.com>
2024-03-01 14:04:43 +0100

    Ticketsystem eingeführt, Ticketklasse umbenannt

---
+++ Fachmod_FIAE2/src/zoo/Model/Ticket.java
@@ -0,0 +1,50 @@
+package zoo.Model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Ticket {
+	private int ticketID;
+	private double price;
+	private String type;
+	private Date date;
+	
+	public Ticket (int ticketID, String type, double price) {
+		this.ticketID = ticketID;
+		this.type = type;
+		this.price = price;
+		this.date = Calendar.getInstance().getTime();
+		}
+	
+	public int getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(int ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setArt(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- Fachmod_FIAE2/src/zoo/Model/Tickets.java
+++
@@ -1,5 +0,0 @@
-package zoo.Model;
-
-public class Tickets {
-
-}
