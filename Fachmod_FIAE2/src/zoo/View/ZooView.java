10c8d3ab03a082a1b482c91bea77211c0b626bee
L2P-Academy <projekte@l2p-academy.com>
2024-03-01 15:37:51 +0100

    View + Layout angepasst, Logo eingefügt, Ordnerstruktur res

--- Fachmod_FIAE2/src/zoo/View/ZooView.java
+++ Fachmod_FIAE2/src/zoo/View/ZooView.java
@@ -6,33 +6,46 @@ import java.awt.event.*;
 
 public class ZooView extends JFrame implements ActionListener {
 
-    private JLabel lblBegruessung;
     private JButton btnEintritt;
     private JPanel pnlHaupt;
+    private ImageIcon imageIcon;
+    private String imgLogoPath;
+    private JLabel lblImageWelcome;
+    private int scaledWidth, scaledHeight;
 
     public ZooView() {
         // Fenster initialisieren
         setTitle("Zoo Eintritt");
-        setSize(1200, 900);
+        setSize(1400, 1100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-        
+        imgLogoPath = "res/img/Zoo_Logo.png";        
 
         // Hauptpanel erstellen
         pnlHaupt = new JPanel();
         pnlHaupt.setLayout(new BorderLayout());
+        
+        // Bild einbinden
+        imageIcon = new ImageIcon(imgLogoPath);
+        Image originalImage = imageIcon.getImage();
+        
+        // Bild skalieren
+        scaledWidth = 1400;
+        scaledHeight = 600;
+        Image scaledImage = originalImage.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        lblImageWelcome = new JLabel(scaledIcon);
+        
+        // Trennlinie einfügen
+        JSeparator separator = new JSeparator();
 
-        // Begrüßungslabel erstellen
-        lblBegruessung = new JLabel("Herzlich willkommen im Zoo!");
-        lblBegruessung.setHorizontalAlignment(SwingConstants.CENTER);
-
-        // Button für den Eintritt erstellen
+        // Button für den Eintritt erstellen -> Anpassen der Größe, Hintergrund, Vordergrund, Schriftfarbe, Art usw.
         btnEintritt = new JButton("Eintritt");
         btnEintritt.addActionListener(this);
 
         // Panel und Button zum Hauptpanel hinzufügen
-        pnlHaupt.add(lblBegruessung, BorderLayout.NORTH);
+        pnlHaupt.add(lblImageWelcome, BorderLayout.NORTH);
         pnlHaupt.add(btnEintritt, BorderLayout.SOUTH);
+        pnlHaupt.add(separator);
 
         // Hauptpanel zum Fenster hinzufügen
         getContentPane().add(pnlHaupt);
