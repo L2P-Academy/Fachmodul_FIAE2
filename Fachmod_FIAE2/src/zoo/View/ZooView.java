7f20bae512be95d01ed92d091a1824dcec8e3972
HackTheWorld <HackTheWorld@DESKTOP-J596C1F.fritz.box>
2024-03-03 18:16:04 +0100

    RadioButtons & Textfield zur Ticketauswahl angelegt.

--- Fachmod_FIAE2/src/zoo/View/ZooView.java
+++ Fachmod_FIAE2/src/zoo/View/ZooView.java
@@ -22,28 +22,72 @@ public class ZooView extends JFrame implements ActionListener {
 
         // Hauptpanel erstellen
         pnlHaupt = new JPanel();
-        pnlHaupt.setLayout(new BorderLayout());
+        pnlHaupt.setLayout(new FlowLayout());
         
         // Bild einbinden
         imageIcon = new ImageIcon(imgLogoPath);
         Image originalImage = imageIcon.getImage();
         
         // Bild skalieren
-        scaledWidth = 1400;
-        scaledHeight = 600;
+        scaledWidth = 1080;
+        scaledHeight = 520;
         Image scaledImage = originalImage.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
         lblImageWelcome = new JLabel(scaledIcon);
         
         // Trennlinie einfügen
         JSeparator separator = new JSeparator();
+        
+        // Radiobuttons für Ticketverkauf deklarieren
+        JRadioButton kinderButton = new JRadioButton("Kinder");
+        JRadioButton erwachseneButton = new JRadioButton("Erwachsene");
+        JRadioButton seniorenButton = new JRadioButton("Senioren");
+        
+        // RadioButtons der ButtonsGroup hinzufügen
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(kinderButton);
+        buttonGroup.add(erwachseneButton);
+        buttonGroup.add(seniorenButton);
+        
+        // Panel für Radiobuttons erstellen
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(1, 3));
+        radioPanel.add(kinderButton);
+        radioPanel.add(erwachseneButton);
+        radioPanel.add(seniorenButton);
+        
+        // Label für "Ticketart"
+        JLabel lblTicketart = new JLabel("Ticketart: ");
+        
+        // Panel für Label und Radiobuttons erstellen
+        JPanel labelRadioPanel = new JPanel();
+        labelRadioPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        labelRadioPanel.add(lblTicketart);
+        labelRadioPanel.add(radioPanel);
+        labelRadioPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 0));
+        
+        // Label für die Anzahl der Tickets
+        JLabel lblAnzahlTickets = new JLabel("Anzahl der Tickets: ");
+        
+        // Eingabefeld für die Ticketanzahl
+        JTextField txtAnzahlTickets = new JTextField(10);
+        
+        // Panel für das Label und das Eingabefeld erstellen
+        JPanel ticketAnzahlPanel = new JPanel(new BorderLayout());
+        ticketAnzahlPanel.add(lblAnzahlTickets, BorderLayout.WEST);
+        ticketAnzahlPanel.add(txtAnzahlTickets, BorderLayout.CENTER);
+        ticketAnzahlPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
 
         // Button für den Eintritt erstellen -> Anpassen der Größe, Hintergrund, Vordergrund, Schriftfarbe, Art usw.
         btnEintritt = new JButton("Eintritt");
+        btnEintritt.setMaximumSize(new Dimension(200, Integer.MAX_VALUE));
         btnEintritt.addActionListener(this);
+
 
         // Panel und Button zum Hauptpanel hinzufügen
         pnlHaupt.add(lblImageWelcome, BorderLayout.NORTH);
+        pnlHaupt.add(labelRadioPanel, BorderLayout.WEST);
+        pnlHaupt.add(ticketAnzahlPanel, BorderLayout.EAST);
         pnlHaupt.add(btnEintritt, BorderLayout.SOUTH);
         pnlHaupt.add(separator);
 
@@ -60,7 +104,7 @@ public class ZooView extends JFrame implements ActionListener {
         JOptionPane.showMessageDialog(this, "Viel Spaß im Zoo!");
     }
 
+    public void displayAnimals() {
+    }
 
-	public void displayAnimals() {
-	}
 }
