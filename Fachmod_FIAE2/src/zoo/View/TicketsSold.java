27a1fa628b762d462cf6b1a782746cc2bd2e4bb9
gerha <gerha@Arbeitstier.fritz.box>
2024-03-11 14:37:00 +0100

    Tabellen angepasst

--- Fachmod_FIAE2/src/zoo/View/TicketsSold.java
+++ Fachmod_FIAE2/src/zoo/View/TicketsSold.java
@@ -20,11 +20,8 @@ public class TicketsSold extends JFrame implements ActionListener {
 	private JLabel lblSouth;
 	private JTable ticketTable;
 	private JPanel pnlAuflistung;
-	private JPanel pnlBerechnung;
-	private JPanel pnlMenge;
 	private JTable auflistung;
-	private JTable gesammtGeld;
-	private JTable gesammtMenge;
+
 
 
 	public TicketsSold() {
@@ -124,21 +121,53 @@ public class TicketsSold extends JFrame implements ActionListener {
 	pnlSouth = new JPanel();
 	pnlSouth.setLayout(new BorderLayout());
 	
-	pnlAuflistung = new JPanel();
-	pnlAuflistung.setLayout(new BorderLayout()); 
+	JSeparator trennLinie = new JSeparator();
 	
-	pnlBerechnung = new JPanel();
-	pnlBerechnung.setLayout(new BorderLayout()); 
-	
-	pnlMenge = new JPanel();
-	pnlMenge.setLayout(new BorderLayout());
 	
 	
 	// Daten für die Tabelle
     String[][] data = {
-            {"1", "Erwachsen", "10", "100", "25.03.2024", "50.0"},
-            {"2", "Kind", "15", "150", "25.03.2024", "75.0"},
-            {"3", "Senioren", "20", "200", "25.03.2024", "100.0"}
+            {"1", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"2", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"3", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"4", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"5", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"6", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"7", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"8", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"9", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"10", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"11", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"12", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"13", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"14", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"15", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"16", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"17", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"18", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"19", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"20", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"21", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"22", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"23", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"24", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"25", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"26", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"27", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"28", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"29", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"30", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"31", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"32", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"33", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"34", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"35", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"36", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"37", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"38", "Kind", "4", "5", "22.03.2024", "75.0"},
+            {"39", "Senioren", "6", "10", "15.03.2024", "100.0"},
+            {"40", "Erwachsen", "2", "3", "02.03.2024", "50.0"},
+            {"41", "Kind", "4", "5", "22.03.2024", "75.0"}
     };
 
     // Spaltenüberschriften
@@ -157,17 +186,30 @@ public class TicketsSold extends JFrame implements ActionListener {
     
  // Daten für die Tabelle
     String[][] daten = {
-            {"Erwachsen"},
-            {"Kind"},
-            {"Senioren"},
-            {"Gesammt"}
+            {"Erwachsen", "2", "", ""},
+            {"Erwachsen", "4", "", ""},
+            {"Erwachsen", "6", "", ""},
+            {"", "", "", ""},
+            {"Kind", "2", "", ""},
+            {"Kind", "4", "", ""},
+            {"Kind", "6", "", ""},
+            {"", "", "", ""},
+            {"Senioren", "2", "", ""},
+            {"Senioren", "4", "", ""},
+            {"Senioren", "6", "", ""},
+            {"", "", "", ""},
+            {"Gesamt nach Dauer", "2", "", ""},
+            {"Gesamt nach Dauer", "4", "", ""},
+            {"Gesamt nach Dauer", "6", "", ""},
+            {"", "", "", ""},
+            {"Gesamt", "", "", ""}
     };
 
     // Spaltenüberschriften
-    String[] ueberschriftWest = {"Art"};
+    String[] ueberschrift = {"Art", "Dauer", "Menge", "Einnahmen"};
 
     // JTable erstellen und speeren der änderung in Zeilen
-    auflistung = new JTable(daten, ueberschriftWest) {
+    auflistung = new JTable(daten, ueberschrift) {
 
 		public boolean isCellEditable(int x, int y) {
             return false;
@@ -177,60 +219,11 @@ public class TicketsSold extends JFrame implements ActionListener {
     // JTable zu JScrollPane hinzufügen
     JScrollPane scrollPaneAuflistung = new JScrollPane(auflistung);
 	
-    // Daten für die Tabelle
-    String[][] menge = {
-            {"230 Stk",},
-            {"170 Stk"},
-            {"50 Stk"},
-            {"450 Stk"}
-    };
-
-    // Spaltenüberschriften
-    String[] ueberschriftCenter = {"Menge"};
-
-    // JTable erstellen und speeren der änderung in Zeilen
-    gesammtMenge = new JTable(menge, ueberschriftCenter) {
-
-		public boolean isCellEditable(int x, int y) {
-            return false;
-        }
-    };
-    
-    // JTable zu JScrollPane hinzufügen
-    JScrollPane scrollPaneMenge = new JScrollPane(gesammtMenge);
-	
-    
-    // Daten für die Tabelle
-    String[][] geld = {
-            {"250 €",},
-            {"150 €"},
-            {"50 €"},
-            {"450 €"}
-    };
-
-    // Spaltenüberschriften
-    String[] ueberschriftEast = {"Euro"};
-
-    // JTable erstellen und speeren der änderung in Zeilen
-    gesammtGeld = new JTable(geld, ueberschriftEast) {
-
-		public boolean isCellEditable(int x, int y) {
-            return false;
-        }
-    };
-
-    // JTable zu JScrollPane hinzufügen
-    JScrollPane scrollPaneGeld = new JScrollPane(gesammtGeld);
-    
-    
+   
     pnlNorth.add(scrollPane, BorderLayout.CENTER);
-    pnlAuflistung.add(scrollPaneAuflistung, BorderLayout.CENTER);
-    pnlMenge.add(scrollPaneMenge, BorderLayout.CENTER);
-    pnlBerechnung.add(scrollPaneGeld, BorderLayout.CENTER);
-	pnlSouth.add(pnlAuflistung, BorderLayout.WEST);
-	pnlSouth.add(pnlBerechnung, BorderLayout.EAST);
-	pnlSouth.add(pnlMenge, BorderLayout.CENTER);
+    pnlSouth.add(scrollPaneAuflistung, BorderLayout.CENTER);
 	pnlMain.add(pnlNorth, BorderLayout.NORTH);
+	pnlMain.add(trennLinie);
 	pnlMain.add(pnlSouth, BorderLayout.SOUTH);
 	
 	
